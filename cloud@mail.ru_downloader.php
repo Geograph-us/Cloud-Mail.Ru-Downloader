a37fe26373f20e270630c7ed40edbe6d13143135
Geograph-us <geograph@list.ru>
2016-08-28 23:09:46 +0500

    Upload

---
+++ cloud@mail.ru_downloader.php
@@ -0,0 +1,154 @@
+<?php
+  $links_file = "links.txt";
+  $storage_path = "downloads";
+
+  $file4aria = "input.txt";
+
+  // ======================================================================================================== //
+
+  if (file_exists($file4aria)) unlink($file4aria);
+  $links = file($links_file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+  echo "Start create input file for Aria2c Downloader..." . PHP_EOL;
+  foreach($links as $link)
+  {
+    $base_url = "";
+    $id = "";
+    if($files = GetAllFiles($link))
+    {
+      foreach ($files as $file)
+      {
+        $line = $file->download_link . PHP_EOL;
+        $line .= "  out=" . $file->output . PHP_EOL;
+        $line .= "  referer=" . $link . PHP_EOL;
+        $line .= "  dir=" . $storage_path . PHP_EOL;
+
+        file_put_contents($file4aria, $line, FILE_APPEND);
+      }
+    }
+  }
+
+  echo "Running Aria2c for download..." . PHP_EOL;
+  StartDownload($file4aria);
+  @unlink($file4aria);
+
+  echo "Done!";
+
+  // ======================================================================================================== //
+
+  class CMFile
+  {
+    public $name = "";
+    public $output = "";
+    public $link = "";
+    public $download_link = "";
+
+    function __construct($name, $output, $link, $download_link)
+    {
+      $this->name = $name;
+      $this->output = $output;
+      $this->link = $link;
+      $this->download_link = $download_link;
+    }
+  }
+
+  // ======================================================================================================== //
+
+  function GetAllFiles($link, $folder = "")
+  {
+    global $base_url, $id;
+
+    $page = get(pathcombine($link, $folder));
+    if ($page === false) { echo "Error $link\r\n"; return false; }
+    if (($mainfolder = GetMainFolder($page)) === false) { echo "Cannot get main folder $link\r\n"; return false; }
+
+    if (!$base_url) $base_url = GetBaseUrl($page);
+    if (!$id && preg_match('~\/public\/(.*)~', $link, $match)) $id = $match[1];
+
+    $cmfiles = array();
+    if ($mainfolder["name"] == "/") $mainfolder["name"] = "";
+    foreach ($mainfolder["list"] as $item)
+    {
+      if ($item["type"] == "folder")
+      {
+        $files_from_folder = GetAllFiles($link, pathcombine($folder, rawurlencode(basename($item["name"]))));
+
+        if (is_array($files_from_folder))
+        {
+          foreach ($files_from_folder as $file)
+          {
+            if ($mainfolder["name"] != "")
+              $file->output = $mainfolder["name"] . "/" . $file->output;
+          }
+          $cmfiles = array_merge($cmfiles, $files_from_folder);
+        }
+      }
+      else
+      {
+        $fileurl = pathcombine($folder, rawurlencode($item["name"]));
+        $cmfiles[] = new CMFile($item["name"],
+                                pathcombine($mainfolder["name"], $item["name"]),
+                                pathcombine($link, $fileurl),
+                                pathcombine($base_url, $id, $fileurl));
+      }
+    }
+
+    return $cmfiles;
+  }
+
+  // ======================================================================================================== //
+
+  function StartDownload($input_file)
+  {
+    $command = "aria2c --file-allocation=none --min-tls-version=TLSv1 --max-connection-per-server=10 --split=10 --max-concurrent-downloads=10 --summary-interval=0 --continue --user-agent=\"Mozilla/5.0 (compatible; Firefox/3.6; Linux)\" --input-file=\"{$input_file}\"";
+    exec("start " . $command);
+  }
+
+  // ======================================================================================================== //
+
+  function GetMainFolder($page)
+  {
+    if (preg_match('~"folder":\s+(\{.*?"id":\s+"[^"]+"\s+\})\s+},undefined\)~s', $page, $match)) return json_decode($match[1], true);
+    else return false;
+  }
+
+  // ======================================================================================================== //
+
+  function GetBaseUrl($page)
+  {
+    if (preg_match('~"weblink_get":.*?"url":\s*"(https:[^"]+)~s', $page, $match)) return $match[1];
+    else return false;
+  }
+
+  // ======================================================================================================== //
+
+  function get($url)
+  {
+    $proxy = null; //"127.0.0.1:8888";
+
+    $http["method"] = "GET";
+    if ($proxy) { $http["proxy"] = "tcp://" . $proxy; $http["request_fulluri"] = true; }
+    $options['http'] = $http;
+    $context = stream_context_create($options);
+    $body = @file_get_contents($url, NULL, $context);
+    return $body;
+  }
+
+  // ======================================================================================================== //
+
+  function pathcombine()
+  {
+    $result = "";
+    foreach (func_get_args() as $arg)
+    {
+        if ($arg !== '')
+        {
+          if ($result && substr($result, -1) != "/") $result .= "/";
+          $result .= $arg;
+        }
+    }
+    return $result;
+  }
+
+  // ======================================================================================================== //
+?>
